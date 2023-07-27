71d4034e9789fbb115f6f21538656f7f9ea2fb53
Sofia17266 <123610145+Sofia17266@users.noreply.github.com>
2023-07-27 11:34:43 -0600

    Actualizacion Proyecto

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@ public class Main{
     
     public static void main(String[] args) {
         Combate combate = new Combate();
+
+
+        int tipoNormal = Pokemones.NORMAL;
+        int tipoFuego = Pokemones.FUEGO;
+        int tipoAgua = Pokemones.AGUA;
+        
+        System.out.println("Información del Pokémon tipo Normal:");
+        System.out.println(Pokemones.obtenerDatosTipo(tipoNormal));
+        
+        System.out.println("\nInformación del Pokémon tipo Fuego:");
+        System.out.println(Pokemones.obtenerDatosTipo(tipoFuego));
+        
+        System.out.println("\nInformación del Pokémon tipo Agua:");
+        System.out.println(Pokemones.obtenerDatosTipo(tipoAgua));
+
     }
 }
